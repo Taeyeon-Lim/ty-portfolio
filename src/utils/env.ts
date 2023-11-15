8b236e63c76235252459f9590e7e62ca23b2b935
Teayeon-Lim <wingofyeon@naver.com>
2023-11-16 01:37:31 +0900

    Add/ new env variables

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,3 +1,11 @@
 export const DOMAIN_URL: string = JSON.parse(
   JSON.stringify(process.env.domainURL)
 );
+
+export const CHANNER_SECRET_KEY = JSON.parse(
+  JSON.stringify(process.env.channerSecretKey)
+);
+
+export const CHNNERTALK_PLUGIN_KEY = JSON.parse(
+  JSON.stringify(process.env.channerTalkPluginKey)
+);
