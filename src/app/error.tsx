f32c10bb3a95f8bfe104de3af8e23e6bad48fc82
Teayeon-Lim <wingofyeon@naver.com>
2023-11-20 17:29:05 +0900

    Add/ home error page

---
+++ src/app/error.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import PageError from '@components/PageError';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  return (
+    <PageError
+      title='Client | Page Load Error'
+      description={{
+        kor: '홈 페이지에 오류가 발생했습니다',
+        eng: 'Home Page Error',
+      }}
+      activeButton={{
+        redirectHome: true,
+        reset,
+      }}
+    />
+  );
+}
