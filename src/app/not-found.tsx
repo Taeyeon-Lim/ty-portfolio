db39fe028e39a5f9b7867bc5ff76d04799e58fda
Teayeon-Lim <wingofyeon@naver.com>
2023-11-20 17:29:17 +0900

    Add/ not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import PageError from '@components/PageError';
+
+export default function NotFound() {
+  return (
+    <PageError
+      title='404 | Not Found Page'
+      description={{
+        kor: '존재 하지 않는 페이지입니다',
+        eng: 'Could not find requested resource',
+      }}
+      activeButton={{
+        redirectHome: true,
+      }}
+    />
+  );
+}
