6540dac81ac8a80b755b56cac0a17e98aa6bc7a6
Teayeon-Lim <wingofyeon@naver.com>
2023-11-20 17:28:12 +0900

    Add/ global-error page

---
+++ src/app/global-error.tsx
@@ -0,0 +1,31 @@
+// Catch-all error handling UI
+
+'use client';
+
+import PageError from '@components/PageError';
+
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  return (
+    <html lang='ko'>
+      <body>
+        <PageError
+          title='500 | Server Error'
+          description={{
+            kor: '웹 어플리케이션에 오류가 발생했습니다',
+            eng: 'Internal Server Error',
+          }}
+          activeButton={{
+            redirectHome: true,
+            reset,
+          }}
+        />
+      </body>
+    </html>
+  );
+}
